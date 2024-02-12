df5d6b80d709abb2f27580141bc98e9a13788643
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-12 15:52:42 +0100

    Initial commit

---
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/HubKitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inetum.realdolmen.hubkitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubKitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubKitBackendApplication.class, args);
+	}
+
+}
---
+++ hub-kit-backend/src/test/java/com/inetum/realdolmen/hubkitbackend/HubKitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.realdolmen.hubkitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubKitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
