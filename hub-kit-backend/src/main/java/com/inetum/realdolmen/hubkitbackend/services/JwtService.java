1be9c4e93768f221985a1c565fa26e54406ff93a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-19 21:10:08 +0100

    Move JWT secret key and signature_algorithm to the application.properties file, change token expiration time, add comments

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/services/JwtService.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/services/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,34 +17,56 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY= "Qc10J0K50p4bX6uY9x/ubf4jTZtc3n6uVNZOnKSrP/yndOFJamS3UG0SXAYtG7ErNtFVQ7+5n8s89F6662w52CUx9eK9iFph/u182kZBVTKH5/r3Zfa+sZZP6CEfJ8Ud5kTjr9yAcdxyfussiB7qvuPdCefyDXPOZsUK8GzEtH43FPGVDwFqj6GTF6VeaMpOsbRYgr7JSkPmDTHn91mQ1JYAvw1Yupd+O2/WCbgahlXqzHz8DA2g30VVoc5l72rc9M3jtK/JtBfyMbBhAGUpMB77a4BU0QTFZe7/dnLbd67QLpfmCvOZEiTXvugA5SEjOJnHabESvV4fFB3l2/46nLrQu8neUJfBLCCAPet5guM=\n";
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.signature-algorithm}")
+    private String signature_algorithm;
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    //used to extract one claim from the claims
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
-        final Claims claims= extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
     //If no extra claims are provided
-    public String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+    //If extra claims are provided
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) //set how long the token is available
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                //The time that the token will be available: 24 hours
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.valueOf(signature_algorithm))
                 .compact();
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails){
-        final String username= extractUsername(token);
+    //Used to extract one claim from the claim's map
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    //Uses the signing key defined to parse the JWT token and extract the claims inside it
+    //The key has to be 256 characters long
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
@@ -55,18 +78,4 @@ public class JwtService {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    //Uses the signing key defined to parse the JWT token and extract the claims inside it
-    //The key has to be 256 characters long
-    private Claims extractAllClaims(String token){
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    private Key getSignInKey(){
-        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
 }
