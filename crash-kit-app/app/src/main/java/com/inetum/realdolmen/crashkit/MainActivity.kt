7ce71124757ad6f141d9ced4d9d7a234e3d350e8
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-22 14:25:17 +0100

    Add clickListener on the register button to redirect to the RegisterActivity

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/MainActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/MainActivity.kt
@@ -32,5 +32,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        binding.btnMainRegisterRedirect.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
