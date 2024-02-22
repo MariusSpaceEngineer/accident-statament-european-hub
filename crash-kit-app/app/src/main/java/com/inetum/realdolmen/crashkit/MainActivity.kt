76618b71b5aa59ce558eeba9e5c17e5c1b232405
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-22 12:28:29 +0100

    Create logic for changing the color of the last word for the tvMainDescription

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/MainActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.inetum.realdolmen.crashkit
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.inetum.realdolmen.crashkit.databinding.ActivityMainBinding
 
@@ -13,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val spannableText = SpannableString("Save time and hassle after a crash")
+        spannableText.setSpan(
+            ForegroundColorSpan(Color.RED), // change the color as needed
+            29, // start index
+            34, // end index
+            Spannable.SPAN_INCLUSIVE_INCLUSIVE
+        )
+        binding.tvMainDescription.text = spannableText
+
 
         binding.btnMainLoginRedirect.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
