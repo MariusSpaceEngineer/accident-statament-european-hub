5884618eafe548876988e1eb34bfc6ecf97ddbbf
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-23 15:12:18 +0100

    1209 Update HomeActivity to make use of a navigation controller

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/HomeActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/HomeActivity.kt
@@ -1,11 +1,22 @@
 package com.inetum.realdolmen.crashkit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
+
+
 }
