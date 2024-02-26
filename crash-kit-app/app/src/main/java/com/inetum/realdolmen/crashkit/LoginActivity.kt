dcb4e3b4f80c1a0cd449d527e8587fdf987b519d
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-26 15:22:58 +0100

    1208 Refactor code to follow the SRP

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/LoginActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 import com.inetum.realdolmen.crashkit.databinding.ActivityLoginBinding
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.json.JSONObject
@@ -21,73 +24,123 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var loadingFragment: LoadingFragment
     private val client = OkHttpClient()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        val fieldEmail = binding.etLoginEmail
-        val fieldPassword = binding.etLoginPassword
-        val loginButton = binding.btnLoginSubmit
 
-        val intent = Intent(this, HomeActivity::class.java)
+        val emailField = binding.etLoginEmail
+        val passwordField = binding.etLoginPassword
+        var fields = mapOf(
+            emailField to getString(R.string.email_required),
+            passwordField to getString(R.string.password_required),
+        )
+
+        val loginButton = binding.btnLoginSubmit
 
         loadingFragment =
             supportFragmentManager.findFragmentById(R.id.fr_login_loading) as LoadingFragment
 
-
         loginButton.setOnClickListener {
-
-            if (fieldEmail.text.toString().trim().isEmpty()) {
-                fieldEmail.error = "Email is required!"
-            }
-            if (fieldPassword.text.toString().trim().isEmpty()) {
-                fieldPassword.error = "Password is required!"
-            }
-            if (fieldEmail.text.toString().trim().isNotEmpty() && fieldPassword.text.toString()
-                    .trim()
-                    .isNotEmpty()
-            ) {
-
-                val jsonObject = JSONObject()
-                jsonObject.put("email", fieldEmail.text.toString())
-                jsonObject.put("password", fieldPassword.text.toString())
-
-                val mediaType = "application/json; charset=utf-8".toMediaType()
-                val body = jsonObject.toString().toRequestBody(mediaType)
-
-                val request = Request.Builder()
-                    .url("https://10.0.2.2:8080/api/v1/auth/login")
-                    .post(body)
-                    .build()
-
-                client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
-
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                            println(response.body!!.string())
-
-                            runOnUiThread {
-                                loadingFragment.showLoadingFragment()
-                                Toast.makeText(this@LoginActivity, "Logged in", Toast.LENGTH_LONG)
-                                    .show()
-
-                                startActivity(intent)
-                            }
-                        }
-                    }
-                })
+            if (areFieldsValid(fields)) {
+                performLogin(emailField, passwordField)
             }
         }
     }
 
+    private fun areFieldsValid(fields: Map<TextInputEditText, String>): Boolean {
+        var allFieldsValid = true
+        for ((field, errorMessage) in fields) {
+            if (field.text.toString().trim().isEmpty()) {
+                field.error = errorMessage
+                allFieldsValid = false
+            }
+        }
+        return allFieldsValid
+    }
+
+    private fun performLogin(emailField: TextInputEditText, passwordField: TextInputEditText) {
+        val jsonObject = createRequestBody(emailField, passwordField)
+        val request = createRequest(jsonObject)
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    createSimpleDialog(
+                        getString(R.string.error),
+                        getString(R.string.network_error)
+                    )
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                handleResponse(response)
+            }
+        })
+    }
+
+    private fun createRequestBody(
+        emailField: TextInputEditText,
+        passwordField: TextInputEditText
+    ): RequestBody {
+        val jsonObject = JSONObject()
+        jsonObject.put("email", emailField.text.toString())
+        jsonObject.put("password", passwordField.text.toString())
+
+        val mediaType = "application/json; charset=utf-8".toMediaType()
+        return jsonObject.toString().toRequestBody(mediaType)
+    }
+
+    private fun createRequest(body: RequestBody): Request {
+        return Request.Builder()
+            .url("https://10.0.2.2:8080/api/v1/auth/login")
+            .post(body)
+            .build()
+    }
+
+    private fun handleResponse(response: Response) {
+        response.use {
+            if (!response.isSuccessful) {
+                val jsonObject = JSONObject(response.body!!.string())
+                val message = jsonObject.getString("errorMessage")
+
+                runOnUiThread {
+                    createSimpleDialog(getString(R.string.error), message)
+                }
+
+            } else {
+
+                runOnUiThread {
+                    loadingFragment.showLoadingFragment()
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Logged in",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+
+                    startActivity(Intent(this, HomeActivity::class.java))
+                }
+            }
+        }
+    }
+
+
     override fun onResume() {
         super.onResume()
         loadingFragment.hideLoadingFragment()
+    }
+
+    private fun createSimpleDialog(title: String, message: String) {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Close") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
