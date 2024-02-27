523c78c00e49d52dc417a85b104b96a920395688
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-27 20:24:43 +0100

    1208 Create SecurePreferences class

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/SecurePreferences.kt
@@ -0,0 +1,54 @@
+package com.inetum.realdolmen.crashkit
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+
+class SecurePreferences(context: Context) {
+
+    private val sharedPreferences: SharedPreferences
+
+    init {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyGenParameterSpec(
+                KeyGenParameterSpec.Builder(
+                    MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                )
+                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                    .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+                    .build()
+            )
+            .build()
+
+        sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            "secure_preferences",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+    fun putString(key: String, value: String) {
+        with(sharedPreferences.edit()) {
+            putString(key, value)
+            commit()
+        }
+    }
+
+    fun getString(key: String): String? {
+        return sharedPreferences.getString(key, null)
+    }
+
+    fun removeString(key: String){
+        with(sharedPreferences.edit()) {
+            remove(key)
+            apply()
+        }
+    }
+}
