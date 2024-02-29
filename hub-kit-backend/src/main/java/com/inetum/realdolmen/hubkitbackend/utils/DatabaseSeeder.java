273411b67502cbc4f5c87d39ce998ee5c62ffe1a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-29 17:26:39 +0100

    1213 Update DatabaseSeeder to seed Insurance Companies, Insurance Agencies and Insurance Certificates

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/utils/DatabaseSeeder.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/utils/DatabaseSeeder.java
@@ -1,8 +1,10 @@
 package com.inetum.realdolmen.hubkitbackend.utils;
 
 import com.inetum.realdolmen.hubkitbackend.Roles;
-import com.inetum.realdolmen.hubkitbackend.models.PolicyHolder;
-import com.inetum.realdolmen.hubkitbackend.models.User;
+import com.inetum.realdolmen.hubkitbackend.models.*;
+import com.inetum.realdolmen.hubkitbackend.repositories.InsuranceAgencyRepository;
+import com.inetum.realdolmen.hubkitbackend.repositories.InsuranceCertificateRepository;
+import com.inetum.realdolmen.hubkitbackend.repositories.InsuranceCompanyRepository;
 import com.inetum.realdolmen.hubkitbackend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +14,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
@@ -20,31 +23,109 @@ import java.util.List;
 public class DatabaseSeeder {
 
     private final JdbcTemplate jdbcTemplate;
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final InsuranceCompanyRepository insuranceCompanyRepository;
+    private final InsuranceAgencyRepository insuranceAgencyRepository;
+    private final InsuranceCertificateRepository insuranceCertificateRepository;
+
 
     @EventListener
     public void seed(ContextRefreshedEvent event) {
-        seedUsersTable();
+        seedInsuranceCompanyTable();
+        seedInsuranceAgencyTable();
+        seedInsuranceCertificateTable();
+        seedPolicyHolderTable();
     }
 
-    private void seedUsersTable() {
-        String sql = "SELECT * FROM users U";
-        List<User> u = jdbcTemplate.query(sql, (resultSet, rowNum) -> null);
-        if (u.isEmpty()) {
+    private void seedInsuranceCompanyTable() {
+        String sql = "SELECT * FROM insurance_companies IC";
+        List<InsuranceCompany> ic = jdbcTemplate.query(sql, (resultSet, rowNum) -> null);
+        if (ic.isEmpty()) {
+            var insuranceCompany = InsuranceCompany.builder()
+                    .name("Ethias")
+                    .build();
 
-            var user = PolicyHolder.builder()
-                    .firstName("John").
-                    lastName("Doe").
-                    email("johndoe@gmail.com")
+            insuranceCompanyRepository.save(insuranceCompany);
+
+            log.info("Insurance Company Seeded");
+        } else {
+            log.info("Insurance Company Seeding Not Required");
+        }
+
+    }
+
+    private void seedInsuranceAgencyTable() {
+        String sql = "SELECT * FROM insurance_agencies IA";
+        List<InsuranceAgency> ia = jdbcTemplate.query(sql, (resultSet, rowNum) -> null);
+        if (ia.isEmpty()) {
+            var insuranceAgency = InsuranceAgency.builder()
+                    .name("Arces")
+                    .address("Desguinlei 92, 2018 Antwerpen")
+                    .country("BE")
+                    .phoneNumber("032591970")
+                    .email("Info@arces.be")
+                    .build();
+
+            insuranceAgencyRepository.save(insuranceAgency);
+
+            log.info("Insurance Agency Seeded");
+        } else {
+            log.info("Insurance Agency Seeding Not Required");
+        }
+
+    }
+
+    private void seedInsuranceCertificateTable() {
+        String sql = "SELECT * FROM insurance_certificates IC";
+        List<InsuranceCertificate> ic = jdbcTemplate.query(sql, (resultSet, rowNum) -> null);
+        if (ic.isEmpty()) {
+            var insuranceCertificate = InsuranceCertificate.builder()
+                    .policyNumber("POL123456789")
+                    .greenCardNumber("GCN987654321")
+                    .insuranceCertificateAvailabilityDate(LocalDate.now())
+                    .insuranceCertificateExpirationDate(LocalDate.of(2025, 2, 28))
+                    .build();
+
+            var insuranceAgency = insuranceAgencyRepository.findByName("Arces");
+            insuranceAgency.ifPresent(insuranceCertificate::setInsuranceAgency);
+
+            var insuranceCompany = insuranceCompanyRepository.findByName("Ethias");
+            insuranceCompany.ifPresent(insuranceCertificate::setInsuranceCompany);
+
+            insuranceCertificateRepository.save(insuranceCertificate);
+
+            log.info("Insurance Certificate Seeded");
+        } else {
+            log.info("Insurance Certificate Seeding Not Required");
+        }
+
+    }
+
+    private void seedPolicyHolderTable() {
+        String sql = "SELECT * FROM policy_holders PH";
+        List<PolicyHolder> ph = jdbcTemplate.query(sql, (resultSet, rowNum) -> null);
+        if (ph.isEmpty()) {
+
+            var policyHolder = PolicyHolder.builder()
+                    .firstName("John")
+                    .lastName("Doe")
+                    .address("Koningin Astridplein 27, 2018 Antwerpen")
+                    .postalCode("2018")
+                    .phoneNumber("0465879425")
+                    .email("johndoe@gmail.com")
                     .role(Roles.POLICY_HOLDER)
                     .password(new BCryptPasswordEncoder().encode("1234"))
                     .build();
 
-            repository.save(user);
+            var insuranceCertificate = insuranceCertificateRepository.findByGreenCardNumberAndPolicyNumber("GCN987654321", "POL123456789");
+            insuranceCertificate.ifPresent(policyHolder::setInsuranceCertificate);
 
-            log.info("Users Seeded");
+            userRepository.save(policyHolder);
+            log.info("Policy Holder Seeded");
         } else {
-            log.info("Users Seeding Not Required");
+            log.info("Policy Holder Seeding Not Required");
         }
     }
+
+
 }
