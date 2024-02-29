b3144f3b30c6729b87abd8569a3af3855f223920
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-02-29 17:24:08 +0100

    1213 Create PolicyHolderRepository

---
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/repositories/PolicyHolderRepository.java
@@ -0,0 +1,8 @@
+package com.inetum.realdolmen.hubkitbackend.repositories;
+
+
+import com.inetum.realdolmen.hubkitbackend.models.PolicyHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PolicyHolderRepository extends JpaRepository<PolicyHolder, Integer> {
+}
