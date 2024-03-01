d8291a626d0b7ed03c2d94b0c608ae2f1132feaf
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-01 23:09:30 +0100

    1208 Implement retrofit2 and gson converter

--- crash-kit-app/app/build.gradle.kts
+++ crash-kit-app/app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation ("com.auth0.android:jwtdecode:2.0.2")
     implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
