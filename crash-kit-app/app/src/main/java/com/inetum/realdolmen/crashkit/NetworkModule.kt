586e273cb5a8b12e771ccc8deed187fdc18963bb
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-01 23:13:07 +0100

    1208 Create NetworkModule

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/NetworkModule.kt
@@ -0,0 +1,40 @@
+package com.inetum.realdolmen.crashkit
+
+import com.inetum.realdolmen.crashkit.services.ApiService
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+
+    private const val BASE_URL = "https://10.0.2.2:8080/api/v1/"
+
+    fun provideOkHttpClient(securePreferences: SecurePreferences): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val originalRequest = chain.request()
+                val jwtToken = securePreferences.getString("jwt_token")
+                val newRequest = if (jwtToken != null) {
+                    originalRequest.newBuilder()
+                        .header("Authorization", "Bearer $jwtToken")
+                        .build()
+                } else {
+                    originalRequest
+                }
+                chain.proceed(newRequest)
+            }
+            .build()
+    }
+
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
