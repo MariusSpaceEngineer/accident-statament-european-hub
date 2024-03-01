5da2ae4d8d4add59910437c292a5774d688c2b77
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-01 23:33:58 +0100

    1213 Update PolicyHolderService to use mappers and create method updateInsuranceCertificateInformation

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/services/PolicyHolderService.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/services/PolicyHolderService.java
@@ -1,10 +1,13 @@
 package com.inetum.realdolmen.hubkitbackend.services;
 
-import com.inetum.realdolmen.hubkitbackend.dto.InsuranceAgencyDTO;
 import com.inetum.realdolmen.hubkitbackend.dto.InsuranceCertificateDTO;
-import com.inetum.realdolmen.hubkitbackend.dto.InsuranceCompanyDTO;
 import com.inetum.realdolmen.hubkitbackend.dto.PolicyHolderDTO;
-import com.inetum.realdolmen.hubkitbackend.models.*;
+import com.inetum.realdolmen.hubkitbackend.dto.PolicyHolderPersonalInformationDTO;
+import com.inetum.realdolmen.hubkitbackend.mappers.InsuranceMapper;
+import com.inetum.realdolmen.hubkitbackend.mappers.PolicyHolderMapper;
+import com.inetum.realdolmen.hubkitbackend.mappers.PolicyHolderPersonalInformationMapper;
+import com.inetum.realdolmen.hubkitbackend.models.PolicyHolder;
+import com.inetum.realdolmen.hubkitbackend.models.User;
 import com.inetum.realdolmen.hubkitbackend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,51 +20,9 @@ public class PolicyHolderService {
     private final UserRepository repository;
     private final JwtService jwtService;
 
-    private PolicyHolderDTO mapPolicyHolderToDto(PolicyHolder policyHolder){
-
-        InsuranceCertificate insuranceCertificate = policyHolder.getInsuranceCertificate();
-
-        InsuranceCompany insuranceCompany = insuranceCertificate.getInsuranceCompany();
-
-        InsuranceCompanyDTO insuranceCompanyDTO = InsuranceCompanyDTO.builder()
-                .id(insuranceCompany.getId())
-                .name(insuranceCompany.getName())
-                .build();
-
-        InsuranceAgency insuranceAgency = insuranceCertificate.getInsuranceAgency();
-
-        InsuranceAgencyDTO insuranceAgencyDTO = InsuranceAgencyDTO.builder()
-                .id(insuranceAgency.getId())
-                .name(insuranceAgency.getName())
-                .address(insuranceAgency.getAddress())
-                .country(insuranceAgency.getCountry())
-                .phoneNumber(insuranceAgency.getPhoneNumber())
-                .email(insuranceAgency.getEmail())
-                .build();
-
-        InsuranceCertificateDTO insuranceCertificateDTO = InsuranceCertificateDTO.builder()
-                .id(insuranceCertificate.getId())
-                .policyNumber(insuranceCertificate.getPolicyNumber())
-                .greenCardNumber(insuranceCertificate.getGreenCardNumber())
-                .availabilityDate(insuranceCertificate.getAvailabilityDate())
-                .expirationDate(insuranceCertificate.getExpirationDate())
-                .insuranceAgency(insuranceAgencyDTO)
-                .insuranceCompany(insuranceCompanyDTO)
-                .build();
-
-        PolicyHolderDTO dto = PolicyHolderDTO.builder()
-                .id(policyHolder.getId())
-                .email(policyHolder.getEmail())
-                .firstName(policyHolder.getFirstName())
-                .lastName(policyHolder.getLastName())
-                .address(policyHolder.getAddress())
-                .postalCode(policyHolder.getPostalCode())
-                .phoneNumber(policyHolder.getPhoneNumber())
-                .insuranceCertificate(insuranceCertificateDTO)
-                .build();
-
-        return dto;
-    }
+    private final PolicyHolderMapper policyHolderMapper;
+    private final InsuranceMapper insuranceMapper;
+    private final PolicyHolderPersonalInformationMapper personalInformationMapper;
 
     public Optional<PolicyHolderDTO> fetchPolicyHolderProfile(String token) {
         try {
@@ -70,7 +31,7 @@ public class PolicyHolderService {
 
             if (user.isPresent()) {
                 PolicyHolder policyHolder = (PolicyHolder) user.get();
-                PolicyHolderDTO dto = mapPolicyHolderToDto(policyHolder);
+                PolicyHolderDTO dto = policyHolderMapper.toDTO(policyHolder);
 
                 return Optional.of(dto);
             } else {
@@ -82,7 +43,7 @@ public class PolicyHolderService {
         }
     }
 
-    public Optional<PolicyHolderDTO> updatePolicyHolderPersonalInformation(String token, PolicyHolderDTO policyHolderDTO) {
+    public Optional<PolicyHolderPersonalInformationDTO> updatePolicyHolderPersonalInformation(String token, PolicyHolderPersonalInformationDTO policyHolderDTO) {
         try {
             String email = jwtService.extractUsername(token);
             Optional<User> user = repository.findByEmail(email);
@@ -90,16 +51,35 @@ public class PolicyHolderService {
             if (user.isPresent()) {
                 PolicyHolder policyHolder = (PolicyHolder) user.get();
 
-                policyHolder.setLastName(policyHolderDTO.getLastName());
-                policyHolder.setFirstName(policyHolderDTO.getFirstName());
-                policyHolder.setAddress(policyHolderDTO.getAddress());
-                policyHolder.setPostalCode(policyHolderDTO.getPostalCode());
-                policyHolder.setPhoneNumber(policyHolder.getPhoneNumber());
-                policyHolder.setEmail(policyHolderDTO.getEmail());
+                personalInformationMapper.updateFromDTO(policyHolderDTO, policyHolder);
 
                 repository.save(policyHolder);
 
-                return Optional.of(mapPolicyHolderToDto(policyHolder));
+                return Optional.of(personalInformationMapper.toDTO(policyHolder));
+
+            } else {
+                return Optional.empty(); // User not found
+            }
+        } catch (Exception e) {
+            //TODO add exception handling
+            return Optional.empty(); // Return empty Optional in case of exceptions
+        }
+    }
+
+    public Optional<InsuranceCertificateDTO> updateInsuranceCertificateInformation(String token, InsuranceCertificateDTO insuranceCertificateDTO) {
+        try {
+            String email = jwtService.extractUsername(token);
+            Optional<User> user = repository.findByEmail(email);
+
+            if (user.isPresent()) {
+                PolicyHolder policyHolder = (PolicyHolder) user.get();
+
+                policyHolder.setInsuranceCertificate(insuranceMapper.updateFromDTO(insuranceCertificateDTO, policyHolder.getInsuranceCertificate()));
+
+                repository.save(policyHolder);
+                InsuranceCertificateDTO result = insuranceMapper.toDTO(policyHolder.getInsuranceCertificate());
+
+                return Optional.of(result);
 
             } else {
                 return Optional.empty(); // User not found
