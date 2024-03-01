dbcd028ac008e345ae8f5fd5c865fba234831b0e
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-01 23:22:21 +0100

    1209 Create RegisterData and RegisterResponse

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/RegisterData.kt
@@ -0,0 +1,8 @@
+package com.inetum.realdolmen.crashkit.dto
+
+data class RegisterData(
+    val firstName:String,
+    val lastName:String,
+    val email: String,
+    val password: String
+)
---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/RegisterResponse.kt
@@ -0,0 +1,6 @@
+package com.inetum.realdolmen.crashkit.dto
+
+data class RegisterResponse(
+    val token: String? = null,
+    val errorMessage: String? = null
+)
