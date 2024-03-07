a53eb9eee7f5b69d0a11f2b27790ea10c120d817
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-07 09:50:25 +0100

    1210 Add logic to check if the user is logged a guest or not and change things dynamically

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/activities/HomeActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/activities/HomeActivity.kt
@@ -5,19 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.inetum.realdolmen.crashkit.CrashKitApp
 import com.inetum.realdolmen.crashkit.R
+import com.inetum.realdolmen.crashkit.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    private val securedPreferences = CrashKitApp.securedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragmentContainerView)
 
         bottomNavigationView.setupWithNavController(navController)
+
+        if (securedPreferences.isGuest()){
+            val menu = binding.bottomNavigationView.menu
+            menu.removeItem(R.id.profileFragment)
+        }
+
     }
-
-
 }
