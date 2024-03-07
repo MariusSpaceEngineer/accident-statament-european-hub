d468c9dab559ace54dea5b20d6e03acb720bac24
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-07 10:00:55 +0100

    1210 Refactor MainActivity to encapsulate code

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/activities/MainActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/activities/MainActivity.kt
@@ -9,9 +9,9 @@ import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import com.auth0.android.jwt.JWT
 import com.inetum.realdolmen.crashkit.CrashKitApp
-import com.inetum.realdolmen.crashkit.fragments.LoadingFragment
 import com.inetum.realdolmen.crashkit.R
 import com.inetum.realdolmen.crashkit.databinding.ActivityMainBinding
+import com.inetum.realdolmen.crashkit.fragments.LoadingFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,12 +19,29 @@ class MainActivity : AppCompatActivity() {
 
     private val securedPreferences = CrashKitApp.securedPreferences
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // If the JWT token is null, expired, or invalid, initialize the login/register components
+        checkLoginStatus()
+
+        loadingFragment =
+            supportFragmentManager.findFragmentById(R.id.fr_main_loading) as LoadingFragment
+
+        //Changes the color of the last word of the description to red
+        changeDescriptionTextToRed()
+
+        setupClickListeners()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadingFragment.hideLoadingFragment()
+    }
+
+    private fun checkLoginStatus() {
         if (securedPreferences.isLoginRemembered()) {
 
             val jwtToken = securedPreferences.getJwtToken()
@@ -39,12 +56,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
         securedPreferences.deleteJwtToken()
+    }
 
-
-        // If the JWT token is null, expired, or invalid, initialize the login/register components
-        loadingFragment =
-            supportFragmentManager.findFragmentById(R.id.fr_main_loading) as LoadingFragment
-
+    private fun changeDescriptionTextToRed() {
         val spannableText = SpannableString("Save time and hassle after a crash")
         spannableText.setSpan(
             ForegroundColorSpan(Color.RED), // change the color as needed
@@ -53,7 +67,9 @@ class MainActivity : AppCompatActivity() {
             Spannable.SPAN_INCLUSIVE_INCLUSIVE
         )
         binding.tvMainDescription.text = spannableText
+    }
 
+    private fun setupClickListeners() {
         binding.btnMainLoginRedirect.setOnClickListener {
             loadingFragment.showLoadingFragment()
             val intent = Intent(this, LoginActivity::class.java)
@@ -72,10 +88,5 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        loadingFragment.hideLoadingFragment()
     }
 }
