6bc76404402c53f12d499f730f30937d1a082b0f
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-07 13:19:48 +0100

    1222 Move NewStatementFragment

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/NewStatementFragment.kt
@@ -0,0 +1,39 @@
+package com.inetum.realdolmen.crashkit.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.inetum.realdolmen.crashkit.R
+import com.inetum.realdolmen.crashkit.databinding.FragmentNewStatementBinding
+
+class NewStatementFragment : Fragment() {
+    private var _binding: FragmentNewStatementBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentNewStatementBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStatementAccidentNext.setOnClickListener {
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.fragmentContainerView, VehicleANewStatementFragment())
+
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+    }
+}
