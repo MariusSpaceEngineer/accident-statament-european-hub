d1310d975893927aa73d37aa7a3d67978d383098
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-08 10:30:29 +0100

    1222 Create VehicleACircumstancesNewStatementFragment

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/VehicleACircumstancesNewStatementFragment.kt
@@ -0,0 +1,44 @@
+package com.inetum.realdolmen.crashkit.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.inetum.realdolmen.crashkit.R
+import com.inetum.realdolmen.crashkit.databinding.FragmentVehicleACircumstancesNewStatementBinding
+
+class VehicleACircumstancesNewStatementFragment : Fragment() {
+
+    private var _binding: FragmentVehicleACircumstancesNewStatementBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentVehicleACircumstancesNewStatementBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStatementAccidentPrevious.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack("VehicleACircumstancesFragment", 0)
+        }
+
+        binding.btnStatementAccidentNext.setOnClickListener {
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+
+            transaction.replace(R.id.fragmentContainerView, VehicleAMiscellaneousNewStatementFragment())
+
+            transaction.addToBackStack("VehicleAMiscellaneousFragment")
+            transaction.commit()
+        }
+
+    }
+}
