3aaa1a2d8daddf653c9457f9acbb8a632013584a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-08 12:33:44 +0100

    1222 Duplicate statement fragments and make changes for vehicle B

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/vehicle_b/VehicleBNewStatementFragment.kt
@@ -0,0 +1,20 @@
+package com.inetum.realdolmen.crashkit.fragments.statement.vehicle_b
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.inetum.realdolmen.crashkit.R
+
+class VehicleBNewStatementFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vehicle_b_new_statement, container, false)
+    }
+
+}
