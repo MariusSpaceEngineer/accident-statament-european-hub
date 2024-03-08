ab0ca8cd3f2ac00ee83b622cb07d21c70740ad8a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-08 10:30:58 +0100

    1222 Create VehicleAMiscellaneousNewStatementFragment

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/VehicleAMiscellaneousNewStatementFragment.kt
@@ -0,0 +1,54 @@
+package com.inetum.realdolmen.crashkit.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.inetum.realdolmen.crashkit.R
+import com.inetum.realdolmen.crashkit.databinding.FragmentVehicleAMiscellaneousNewStatementBinding
+
+
+class VehicleAMiscellaneousNewStatementFragment : Fragment() {
+    class VehicleACircumstancesNewStatementFragment : Fragment() {
+
+        private var _binding: FragmentVehicleAMiscellaneousNewStatementBinding? = null
+        private val binding get() = _binding!!
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            _binding =
+                FragmentVehicleAMiscellaneousNewStatementBinding.inflate(inflater, container, false)
+            val view = binding.root
+
+            return view
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            binding.btnStatementAccidentPrevious.setOnClickListener {
+                requireActivity().supportFragmentManager.popBackStack(
+                    "VehicleAMiscellaneousFragment",
+                    0
+                )
+            }
+
+            binding.btnStatementAccidentNext.setOnClickListener {
+                val transaction = requireActivity().supportFragmentManager.beginTransaction()
+
+                transaction.replace(
+                    R.id.fragmentContainerView,
+                    VehicleADriverNewStatementFragment()
+                )
+
+                transaction.addToBackStack("VehicleBInsuranceFragment")
+                transaction.commit()
+            }
+
+        }
+    }
+}
