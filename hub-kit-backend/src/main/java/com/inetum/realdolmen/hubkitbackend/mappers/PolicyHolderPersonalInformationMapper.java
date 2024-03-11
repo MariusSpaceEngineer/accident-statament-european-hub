670b3b44005bc9eef1b65d6749a2a81f11fda1fd
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-11 14:53:10 +0100

    1222 Remove unused imports

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/mappers/PolicyHolderPersonalInformationMapper.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/mappers/PolicyHolderPersonalInformationMapper.java
@@ -1,6 +1,5 @@
 package com.inetum.realdolmen.hubkitbackend.mappers;
 
-import com.inetum.realdolmen.hubkitbackend.dto.PolicyHolderDTO;
 import com.inetum.realdolmen.hubkitbackend.dto.PolicyHolderPersonalInformationDTO;
 import com.inetum.realdolmen.hubkitbackend.models.PolicyHolder;
 import org.mapstruct.Mapper;
