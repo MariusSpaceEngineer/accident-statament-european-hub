010c05d2cc10fdd55dc22fd28bfb93b4a39bbfb8
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-11 15:07:17 +0100

    1222 Create AccidentStatementCreationException

---
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/exceptions/AccidentStatementCreationFailed.java
@@ -0,0 +1,7 @@
+package com.inetum.realdolmen.hubkitbackend.exceptions;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class AccidentStatementCreationFailed extends Exception{
+}
