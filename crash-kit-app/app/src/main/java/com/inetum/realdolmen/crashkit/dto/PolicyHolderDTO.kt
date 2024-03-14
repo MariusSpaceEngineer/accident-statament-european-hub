a7e96c377f5d5b0b055583cc0f6f88b0e3cd9538
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-14 21:16:09 +0100

    1222 Update PolicyHolderDTO to contain optional properties

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/PolicyHolderDTO.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/PolicyHolderDTO.kt
@@ -1,10 +1,11 @@
 package com.inetum.realdolmen.crashkit.dto
 
-data class PolicyHolderDTO (
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val phoneNumber: String,
-    val address: String,
-    val postalCode: String
+data class PolicyHolderDTO(
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val email: String? = null,
+    val phoneNumber: String? = null,
+    val address: String? = null,
+    val postalCode: String? = null,
+    val insuranceCertificate: InsuranceCertificate? = null
 )
