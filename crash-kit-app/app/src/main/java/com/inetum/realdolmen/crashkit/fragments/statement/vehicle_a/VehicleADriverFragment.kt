88937d5648a04caf3b1b081238b9125b666ead9b
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-14 10:06:55 +0100

    1222 Implement MaterialDesign's DatePicker

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/vehicle_a/VehicleADriverFragment.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/vehicle_a/VehicleADriverFragment.kt
@@ -7,12 +7,19 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.inetum.realdolmen.crashkit.utils.NewStatementViewModel
 import com.inetum.realdolmen.crashkit.R
 import com.inetum.realdolmen.crashkit.databinding.FragmentVehicleADriverBinding
 import com.inetum.realdolmen.crashkit.helpers.FragmentNavigationHelper
 import com.inetum.realdolmen.crashkit.utils.StatementDataHandler
 import com.inetum.realdolmen.crashkit.utils.printBackStack
+import com.inetum.realdolmen.crashkit.utils.to24Format
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 
 class VehicleADriverFragment : Fragment(), StatementDataHandler {
     private lateinit var model: NewStatementViewModel
@@ -23,6 +30,40 @@ class VehicleADriverFragment : Fragment(), StatementDataHandler {
     private val fragmentNavigationHelper by lazy {
         FragmentNavigationHelper(requireActivity().supportFragmentManager)
     }
+
+    private val changeSupport = PropertyChangeSupport(this)
+
+    private var drivingLicenseExpirationDate: LocalDate? = null
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+
+            // Notify listeners about the change
+            changeSupport.firePropertyChange(
+                "drivingLicenseExpirationDate",
+                oldValue,
+                newValue
+            )
+        }
+
+    private var driverDateOfBirth: LocalDate? = null
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+
+            // Notify listeners about the change
+            changeSupport.firePropertyChange(
+                "driverDateOfBirth",
+                oldValue,
+                newValue
+            )
+        }
+
+    private val datePicker = MaterialDatePicker.Builder.datePicker()
+        .setTitleText("Select date")
+        .build()
+
+    private var currentPicker: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +105,37 @@ class VehicleADriverFragment : Fragment(), StatementDataHandler {
                 "vehicle_a_circumstances_fragment"
             )
         }
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val selectedDate =
+                Instant.ofEpochMilli(selection).atZone(ZoneId.systemDefault()).toLocalDate()
+            when (currentPicker) {
+                "driving_license_date_picker" -> {
+                    drivingLicenseExpirationDate = selectedDate
+                    binding.etStatementVehicleADriverDrivingLicenseExpirationDate.setText(
+                        (drivingLicenseExpirationDate?.to24Format() ?: "")
+                    )
+                }
+
+                "date_of_birth_date_picker" -> {
+                    driverDateOfBirth = selectedDate
+                    binding.etStatementVehicleADriverDateOfBirth.setText(
+                        (driverDateOfBirth?.to24Format() ?: "")
+                    )
+                }
+            }
+        }
+
+        binding.btnDrivingLicenseExpirationDatePicker.setOnClickListener {
+            currentPicker = "driving_license_date_picker"
+            datePicker.show(parentFragmentManager, currentPicker)
+        }
+
+        binding.btnDateOfBirthDatePicker.setOnClickListener {
+            currentPicker = "date_of_birth_date_picker"
+            datePicker.show(parentFragmentManager, currentPicker)
+        }
+
     }
 
     override fun updateUIFromViewModel(model: NewStatementViewModel) {
@@ -71,13 +143,18 @@ class VehicleADriverFragment : Fragment(), StatementDataHandler {
             // Update the UI here based on the new statementData
             binding.etStatementVehicleADriverName.setText(statementData.vehicleADriverLastName)
             binding.etStatementVehicleADriverFirstName.setText(statementData.vehicleADriverFirstName)
-            binding.etStatementVehicleADriverDateOfBirth.setText(statementData.vehicleADriverDateOfBirth)
+            binding.etStatementVehicleADriverDateOfBirth.setText(
+                statementData.vehicleADriverDateOfBirth?.to24Format() ?: ""
+            )
             binding.etStatementVehicleADriverAddress.setText(statementData.vehicleADriverAddress)
             binding.etStatementVehicleADriverCountry.setText(statementData.vehicleADriverCountry)
-            binding.etStatementVehicleAInsuranceAgencyPhoneNumber.setText(statementData.vehicleADriverPhoneNumber)
+            binding.etStatementVehicleADriverPhoneNumber.setText(statementData.vehicleADriverPhoneNumber)
             binding.etStatementVehicleADriverEmail.setText(statementData.vehicleADriverEmail)
             binding.etStatementVehicleADriverDrivingLicenseNumber.setText(statementData.vehicleADriverDrivingLicenseNr)
-            binding.etStatementVehicleADriverDrivingLicenseExpirationDate.setText(statementData.vehicleADriverDrivingLicenseExpirationDate)
+            binding.etStatementVehicleADriverDrivingLicenseExpirationDate.setText(
+                statementData.vehicleADriverDrivingLicenseExpirationDate?.to24Format()
+                    ?: ""
+            )
         })
     }
 
@@ -87,18 +164,21 @@ class VehicleADriverFragment : Fragment(), StatementDataHandler {
             this.vehicleADriverFirstName =
                 binding.etStatementVehicleADriverFirstName.text.toString()
             this.vehicleADriverDateOfBirth =
-                binding.etStatementVehicleADriverDateOfBirth.text.toString()
+                driverDateOfBirth
             this.vehicleADriverAddress =
                 binding.etStatementVehicleADriverAddress.text.toString()
             this.vehicleADriverCountry =
                 binding.etStatementVehicleADriverCountry.text.toString()
             this.vehicleADriverPhoneNumber =
-                binding.etStatementVehicleAInsuranceAgencyPhoneNumber.text.toString()
+                binding.etStatementVehicleADriverPhoneNumber.text.toString()
             this.vehicleADriverEmail = binding.etStatementVehicleADriverEmail.text.toString()
             this.vehicleADriverDrivingLicenseNr =
                 binding.etStatementVehicleADriverDrivingLicenseNumber.text.toString()
-            this.vehicleADriverDrivingLicenseExpirationDate =
-                binding.etStatementVehicleADriverDrivingLicenseExpirationDate.text.toString()
+            this.vehicleADriverDrivingLicenseExpirationDate = drivingLicenseExpirationDate
         }
+    }
+
+    private fun addDateChangeListener(listener: PropertyChangeListener) {
+        changeSupport.addPropertyChangeListener(listener)
     }
 }
--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/vehicle_b/VehicleBDriverFragment.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/vehicle_b/VehicleBDriverFragment.kt
@@ -7,12 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.inetum.realdolmen.crashkit.R
 import com.inetum.realdolmen.crashkit.databinding.FragmentVehicleBDriverBinding
 import com.inetum.realdolmen.crashkit.helpers.FragmentNavigationHelper
 import com.inetum.realdolmen.crashkit.utils.NewStatementViewModel
 import com.inetum.realdolmen.crashkit.utils.StatementDataHandler
 import com.inetum.realdolmen.crashkit.utils.printBackStack
+import com.inetum.realdolmen.crashkit.utils.to24Format
+import java.beans.PropertyChangeSupport
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 
 
 class VehicleBDriverFragment : Fragment(), StatementDataHandler {
@@ -24,6 +30,40 @@ class VehicleBDriverFragment : Fragment(), StatementDataHandler {
     private val fragmentNavigationHelper by lazy {
         FragmentNavigationHelper(requireActivity().supportFragmentManager)
     }
+
+    private val changeSupport = PropertyChangeSupport(this)
+
+    private var drivingLicenseExpirationDate: LocalDate? = null
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+
+            // Notify listeners about the change
+            changeSupport.firePropertyChange(
+                "drivingLicenseExpirationDate",
+                oldValue,
+                newValue
+            )
+        }
+
+    private var driverDateOfBirth: LocalDate? = null
+        set(newValue) {
+            val oldValue = field
+            field = newValue
+
+            // Notify listeners about the change
+            changeSupport.firePropertyChange(
+                "driverDateOfBirth",
+                oldValue,
+                newValue
+            )
+        }
+
+    private val datePicker = MaterialDatePicker.Builder.datePicker()
+        .setTitleText("Select date")
+        .build()
+
+    private var currentPicker: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,19 +106,54 @@ class VehicleBDriverFragment : Fragment(), StatementDataHandler {
                 "vehicle_b_circumstances_fragment"
             )
         }
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val selectedDate =
+                Instant.ofEpochMilli(selection).atZone(ZoneId.systemDefault()).toLocalDate()
+            when (currentPicker) {
+                "driving_license_date_picker" -> {
+                    drivingLicenseExpirationDate = selectedDate
+                    binding.etStatementVehicleBDrivingLicenseExpirationDate.setText(
+                        (drivingLicenseExpirationDate?.to24Format() ?: "")
+                    )
+                }
+
+                "date_of_birth_date_picker" -> {
+                    driverDateOfBirth = selectedDate
+                    binding.etStatementVehicleBDriverDateOfBirth.setText(
+                        (driverDateOfBirth?.to24Format() ?: "")
+                    )
+                }
+            }
+        }
+
+        binding.btnDrivingLicenseExpirationDatePicker.setOnClickListener {
+            currentPicker = "driving_license_date_picker"
+            datePicker.show(parentFragmentManager, currentPicker)
+        }
+
+        binding.btnDateOfBirthDatePicker.setOnClickListener {
+            currentPicker = "date_of_birth_date_picker"
+            datePicker.show(parentFragmentManager, currentPicker)
+        }
     }
 
     override fun updateUIFromViewModel(model: NewStatementViewModel) {
         model.statementData.observe(viewLifecycleOwner, Observer { statementData ->
             binding.etStatementVehicleBDriverName.setText(statementData.vehicleBDriverLastName)
             binding.etStatementVehicleBDriverFirstName.setText(statementData.vehicleBDriverFirstName)
-            binding.etStatementVehicleBDriverDateOfBirth.setText(statementData.vehicleBDriverDateOfBirth)
+            binding.etStatementVehicleBDriverDateOfBirth.setText(
+                statementData.vehicleBDriverDateOfBirth?.to24Format() ?: ""
+            )
             binding.etStatementVehicleBDriverAddress.setText(statementData.vehicleBDriverAddress)
             binding.etStatementVehicleBDriverCountry.setText(statementData.vehicleBDriverCountry)
-            binding.etStatementVehicleBInsuranceAgencyPhoneNumber.setText(statementData.vehicleBDriverPhoneNumber)
+            binding.etStatementVehicleBDriverPhoneNumber.setText(statementData.vehicleBDriverPhoneNumber)
             binding.etStatementVehicleBDriverEmail.setText(statementData.vehicleBDriverEmail)
             binding.etStatementVehicleBDriverDrivingLicenseNumber.setText(statementData.vehicleBDriverDrivingLicenseNr)
-            binding.etStatementVehicleBDriverDrivingLicenseExpirationDate.setText(statementData.vehicleBDriverDrivingLicenseExpirationDate)
+            binding.etStatementVehicleBDrivingLicenseExpirationDate.setText(
+                statementData.vehicleBDriverDrivingLicenseExpirationDate?.to24Format()
+                    ?: ""
+            )
         })
     }
 
@@ -88,18 +163,18 @@ class VehicleBDriverFragment : Fragment(), StatementDataHandler {
             this.vehicleBDriverFirstName =
                 binding.etStatementVehicleBDriverFirstName.text.toString()
             this.vehicleBDriverDateOfBirth =
-                binding.etStatementVehicleBDriverDateOfBirth.text.toString()
+                driverDateOfBirth
             this.vehicleBDriverAddress =
                 binding.etStatementVehicleBDriverAddress.text.toString()
             this.vehicleBDriverCountry =
                 binding.etStatementVehicleBDriverCountry.text.toString()
             this.vehicleBDriverPhoneNumber =
-                binding.etStatementVehicleBInsuranceAgencyPhoneNumber.text.toString()
+                binding.etStatementVehicleBDriverPhoneNumber.text.toString()
             this.vehicleBDriverEmail = binding.etStatementVehicleBDriverEmail.text.toString()
             this.vehicleBDriverDrivingLicenseNr =
                 binding.etStatementVehicleBDriverDrivingLicenseNumber.text.toString()
-            this.vehicleBDriverDrivingLicenseExpirationDate =
-                binding.etStatementVehicleBDriverDrivingLicenseExpirationDate.text.toString()
+            this.vehicleBDriverDrivingLicenseExpirationDate = drivingLicenseExpirationDate
+
         }
     }
 }
