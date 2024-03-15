308753869941ff339067d91c38d23a55dbc4674a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-15 16:07:21 +0100

    1222 Update VehicleNewStatementFragmentInstrumentedUITest class annotation

--- crash-kit-app/app/src/androidTest/java/com/inetum/realdolmen/crashkit/fragments/VehicleNewStatementFragmentInstrumentedUITest.kt
+++ crash-kit-app/app/src/androidTest/java/com/inetum/realdolmen/crashkit/fragments/VehicleNewStatementFragmentInstrumentedUITest.kt
@@ -9,6 +9,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import com.inetum.realdolmen.crashkit.R
@@ -16,8 +17,10 @@ import com.inetum.realdolmen.crashkit.fragments.statement.vehicle_a.VehicleANewS
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 
 
+@RunWith(AndroidJUnit4::class)
 class VehicleNewStatementFragmentInstrumentedUITest {
     private lateinit var device: UiDevice
     private var originalWindowAnimationScale: String = ""
