aa21c58a6777ad3724a02bc9eff499f245b1504d
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-18 11:32:06 +0100

    1216 Create logic to fetch the data from the API and convert it into a QR-code

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/ShareInsuranceInformationFragment.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/ShareInsuranceInformationFragment.kt
@@ -1,15 +1,82 @@
 package com.inetum.realdolmen.crashkit.fragments
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.QRCodeWriter
+import com.inetum.realdolmen.crashkit.CrashKitApp
+import com.inetum.realdolmen.crashkit.R
 import com.inetum.realdolmen.crashkit.databinding.FragmentShareInsuranceInformationBinding
+import com.inetum.realdolmen.crashkit.dto.PolicyHolderResponse
+import com.inetum.realdolmen.crashkit.utils.createSimpleDialog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+
 
 class ShareInsuranceInformationFragment : Fragment() {
     private var _binding: FragmentShareInsuranceInformationBinding? = null
     private val binding get() = _binding!!
+
+    private val apiService = CrashKitApp.apiService
+
+    private fun handlePolicyHolderProfileResponse(response: Response<PolicyHolderResponse>) {
+        Log.i("Request", "Request code: ${response.code()}")
+        if (response.isSuccessful) {
+            val personalInformationResponse = response.body()
+            if (personalInformationResponse != null) {
+                val gson = Gson()
+                val json = gson.toJson(personalInformationResponse)
+
+                val qrCodeWriter = QRCodeWriter()
+                val width = 1000
+                val height = 1000
+                var bitMatrix: BitMatrix? = null
+
+                try {
+                    val hints: MutableMap<EncodeHintType, Any?> = HashMap()
+                    hints[EncodeHintType.CHARACTER_SET] = "UTF-8"
+                    bitMatrix =
+                        qrCodeWriter.encode(json, BarcodeFormat.QR_CODE, width, height, hints)
+                } catch (e: WriterException) {
+                    e.printStackTrace()
+                }
+
+                val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+                for (x in 0 until width) {
+                    for (y in 0 until height) {
+                        bitmap.setPixel(x, y, if (bitMatrix!![x, y]) Color.BLACK else Color.WHITE)
+                    }
+                }
+                binding.ivShareInsuranceQrCode.visibility = View.VISIBLE
+                binding.ivShareInsuranceQrCode.setImageBitmap(bitmap)
+                binding.tvShareInsuranceQrCodeDescription.visibility = View.VISIBLE
+
+                Toast.makeText(
+                    requireContext(),
+                    "Import successful",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+            } else {
+                val errorMessage = "Error while fetching insurance information"
+                requireContext().createSimpleDialog(getString(R.string.error), errorMessage)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +95,15 @@ class ShareInsuranceInformationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnShareInsuranceGenerateQrCode.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = apiService.getPolicyHolderProfileInformation()
+                withContext(Dispatchers.Main) {
+                    handlePolicyHolderProfileResponse(response)
+                }
+            }
+        }
     }
 
 }
