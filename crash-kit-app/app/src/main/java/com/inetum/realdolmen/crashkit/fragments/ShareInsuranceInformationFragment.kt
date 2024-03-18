36547cad27d96c2c575c765fcc3cfa565badcdd8
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-18 10:45:58 +0100

    1216 Create ShareInsuranceInformationFragment

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/ShareInsuranceInformationFragment.kt
@@ -0,0 +1,33 @@
+package com.inetum.realdolmen.crashkit.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.inetum.realdolmen.crashkit.databinding.FragmentShareInsuranceInformationBinding
+
+class ShareInsuranceInformationFragment : Fragment() {
+    private var _binding: FragmentShareInsuranceInformationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentShareInsuranceInformationBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
