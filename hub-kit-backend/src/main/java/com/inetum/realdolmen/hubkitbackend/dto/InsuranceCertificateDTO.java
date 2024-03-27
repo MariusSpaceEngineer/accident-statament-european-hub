5da190224aa01556e714a7864de389581155fda8
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-03-27 15:00:25 +0100

    1216 Make properties not nullable in InsuranceCertificateDTO and PolicyHolderPersonalInformationDTO

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/InsuranceCertificateDTO.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/InsuranceCertificateDTO.java
@@ -2,6 +2,7 @@ package com.inetum.realdolmen.hubkitbackend.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -11,10 +12,16 @@ import java.time.LocalDate;
 @Builder
 public class InsuranceCertificateDTO {
     private Integer id;
+    @NonNull
     private String policyNumber;
+    @NonNull
     private String greenCardNumber;
+    @NonNull
     private LocalDate availabilityDate;
+    @NonNull
     private LocalDate expirationDate;
+    @NonNull
     private InsuranceAgencyDTO insuranceAgency;
+    @NonNull
     private InsuranceCompanyDTO insuranceCompany;
 }
--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/PolicyHolderPersonalInformationDTO.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/PolicyHolderPersonalInformationDTO.java
@@ -2,6 +2,7 @@ package com.inetum.realdolmen.hubkitbackend.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
@@ -9,10 +10,16 @@ import lombok.Setter;
 @Builder
 public class PolicyHolderPersonalInformationDTO {
     private Integer id;
+    @NonNull
     private String firstName;
+    @NonNull
     private String lastName;
+    @NonNull
     private String email;
+    @NonNull
     private String address;
+    @NonNull
     private String postalCode;
+    @NonNull
     private String phoneNumber;
 }
