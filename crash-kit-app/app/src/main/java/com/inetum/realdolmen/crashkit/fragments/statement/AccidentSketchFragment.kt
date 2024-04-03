15d6af739fdb670a062962942063fade09864165
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-03 13:29:49 +0200

    1221 Implement logic in AccidentSketchFragment to open a dialog which contains the icons for the sketch

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/AccidentSketchFragment.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/fragments/statement/AccidentSketchFragment.kt
@@ -1,20 +1,55 @@
 package com.inetum.realdolmen.crashkit.fragments.statement
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.inetum.realdolmen.crashkit.R
+import com.inetum.realdolmen.crashkit.SketchView
+import com.inetum.realdolmen.crashkit.adapters.ShapesAdapter
+import com.inetum.realdolmen.crashkit.databinding.FragmentAccidentSketchBinding
 
 class AccidentSketchFragment : Fragment() {
+
+    private var _binding: FragmentAccidentSketchBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var sketchView: SketchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accident_sketch, container, false)
+        _binding = FragmentAccidentSketchBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        sketchView = view.findViewById(R.id.sketchView)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAccidentSketchSearchShape.setOnClickListener {
+            val dialog = Dialog(requireContext(), R.style.Theme_CrashKit)
+            val recyclerView = RecyclerView(requireContext())
+            recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+            recyclerView.adapter = ShapesAdapter { drawableResId ->
+                sketchView.addShape(drawableResId)
+                dialog.dismiss()
+            }
+
+            dialog.setContentView(recyclerView)
+            dialog.show()
+        }
+
+
     }
 
 }
