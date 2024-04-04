0983bb3d58b0f5edbbf5394eaedee052c0865c72
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-04 11:28:39 +0200

    1221 Create custom Drawable that is rotatable

---
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/RotatableDrawable.kt
@@ -0,0 +1,32 @@
+package com.inetum.realdolmen.crashkit
+
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.DrawableWrapper
+import android.util.Log
+
+class RotatableDrawable(private val drawable: Drawable) : DrawableWrapper(drawable) {
+    private var rotation = 0f
+
+
+    fun setRotation(rotation: Float) {
+        Log.d("RotatableDrawable", "Setting rotation to $rotation")
+        this.rotation = rotation
+        invalidateSelf()
+    }
+
+    override fun draw(canvas: Canvas) {
+        Log.d("RotatableDrawable", "Drawing with rotation $rotation")
+        val saveCount = canvas.save()
+
+        val bounds = bounds
+        val cx = bounds.exactCenterX()
+        val cy = bounds.exactCenterY()
+        canvas.rotate(rotation, cx, cy)
+
+        drawable.draw(canvas)
+
+        canvas.restoreToCount(saveCount)
+    }
+
+}
