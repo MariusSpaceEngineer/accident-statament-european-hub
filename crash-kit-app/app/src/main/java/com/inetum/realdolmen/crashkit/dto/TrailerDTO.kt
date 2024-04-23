a7411ce8e6e57bb35a9419761e01c62c0a656782
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-23 11:41:00 +0200

    1223 Update TrailerDTO properties: add hasRegistration property

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/TrailerDTO.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/TrailerDTO.kt
@@ -1,6 +1,7 @@
 package com.inetum.realdolmen.crashkit.dto
 
 data class TrailerDTO(
+    val hasRegistration: Boolean? = false,
     val licensePlate: String? = null,
     val countryOfRegistration: String? = null
 )
