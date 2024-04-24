1b1f7c1bb3f7175ee0b3c1988d03575a6a2c6d54
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-24 15:18:36 +0200

    1749 Create VehicleRepository

---
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/repositories/VehicleRepository.java
@@ -0,0 +1,10 @@
+package com.inetum.realdolmen.hubkitbackend.repositories;
+
+import com.inetum.realdolmen.hubkitbackend.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+    Optional<Vehicle> findVehicleByLicensePlate(String lia);
+}
