c0582ee277ac75a1008643b940c588e1d6f59f36
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-24 15:19:20 +0200

    1749 Fix typo in parameter name

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/repositories/VehicleRepository.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/repositories/VehicleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
-    Optional<Vehicle> findVehicleByLicensePlate(String lia);
+    Optional<Vehicle> findVehicleByLicensePlate(String licensePlate);
 }
