e54598dbdd127b8c87aec486b635dd1ac091c57d
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-25 18:05:00 +0200

    1749 Encapsulating logic in AccidentStatementService

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/services/AccidentStatementService.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/services/AccidentStatementService.java
@@ -1,11 +1,12 @@
 package com.inetum.realdolmen.hubkitbackend.services;
 
-import com.inetum.realdolmen.hubkitbackend.dto.AccidentStatementDTO;
-import com.inetum.realdolmen.hubkitbackend.dto.LocationCoordinates;
+import com.inetum.realdolmen.hubkitbackend.dto.*;
 import com.inetum.realdolmen.hubkitbackend.exceptions.AccidentStatementCreationFailed;
 import com.inetum.realdolmen.hubkitbackend.exceptions.FetchLocationAddressFailedException;
 import com.inetum.realdolmen.hubkitbackend.exceptions.MissingPropertyException;
 import com.inetum.realdolmen.hubkitbackend.mappers.AccidentStatementMapper;
+import com.inetum.realdolmen.hubkitbackend.mappers.MotorMapper;
+import com.inetum.realdolmen.hubkitbackend.mappers.TrailerMapper;
 import com.inetum.realdolmen.hubkitbackend.models.*;
 import com.inetum.realdolmen.hubkitbackend.repositories.*;
 import com.opencagedata.jopencage.JOpenCageGeocoder;
@@ -15,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -30,7 +33,11 @@ public class AccidentStatementService {
     private final InsuranceAgencyRepository insuranceAgencyRepository;
     private final InsuranceCompanyRepository insuranceCompanyRepository;
     private final PolicyHolderRepository policyHolderRepository;
+
     private final AccidentStatementMapper accidentStatementMapper;
+    private final MotorMapper motorMapper;
+    private final TrailerMapper trailerMapper;
+
     @Value("${open-cage-api.key}")
     private String openCageApiKey;
     private JOpenCageGeocoder jOpenCageGeocoder;
@@ -45,39 +52,13 @@ public class AccidentStatementService {
         try {
 
             AccidentStatement accidentStatement = accidentStatementMapper.fromDTO(accidentStatementDTO);
+            assignAndMapVehicleDTOsToAccidentStatement(accidentStatementDTO, accidentStatement);
 
-            //Save accident images
-            if (accidentStatement.getVehicleAAccidentImages() != null) {
-                accidentImageRepository.saveAll(accidentStatement.getVehicleAAccidentImages());
-            }
-            if (accidentStatement.getVehicleBAccidentImages() != null) {
-                accidentImageRepository.saveAll(accidentStatement.getVehicleBAccidentImages());
-            }
+            saveAccidentImages(accidentStatement);
 
-            //Save drivers
-            for (Driver driver : accidentStatement.getDrivers()) {
-                var existingDriver = driverRepository.findByDrivingLicenseNr(driver.getDrivingLicenseNr());
-                if (existingDriver.isPresent()) {
-                    var index = accidentStatement.getDrivers().indexOf(driver);
-                    accidentStatement.getDrivers().set(index, existingDriver.get());
-                } else {
-                    driverRepository.save(driver);
-                }
-            }
+            saveDrivers(accidentStatement);
 
-            //Save witness
-            var witness = accidentStatement.getWitness();
-            if (witness != null) {
-                if (witness.getName() == null || witness.getName().isEmpty() || witness.getAddress() == null || witness.getAddress().isEmpty()) {
-                    throw new MissingPropertyException("Witness's name and address cannot be null");
-                }
-                var existingWitness = witnessRepository.findByNameAndAddress(witness.getName(), witness.getAddress());
-                if (existingWitness.isPresent()) {
-                    accidentStatement.setWitness(existingWitness.get());
-                } else {
-                    witnessRepository.save(witness);
-                }
-            }
+            saveWitness(accidentStatement);
 
             //Save motors
             if (accidentStatement.getMotors() != null) {
@@ -110,7 +91,16 @@ public class AccidentStatementService {
             }
 
             //TODO check which fields can be null in policyHolder, InsuranceCertificate, InsuranceAgency and InsuranceCompany
-            //Save policy holders
+            //TODO make sure the loop goes over all insurance certificates of a policy holder + assign the vehicles to it and check if they exist
+
+
+            //Each policyHolder has insuranceCertificates, if those insuranceCertificates exist they have to be updated
+            //If they don't exist they have to be created
+            //If the policyholder exists and the insuranceCertificate is assigned to him also, they both have to be updated
+            //If the policyholder exists and the insuranceCertificate is not assigned to him, it has to be assigned
+            //If the policyholder doesn't exist it has to be made and the insuranceCertificate assigned to him
+
+            // Save insurance certificates
             for (PolicyHolder policyHolder : accidentStatement.getPolicyHolders()) {
                 var insuranceCertificate = policyHolder.getInsuranceCertificates().getFirst();
                 if (insuranceCertificate.getGreenCardNumber() == null || insuranceCertificate.getGreenCardNumber().isEmpty() || insuranceCertificate.getPolicyNumber() == null || insuranceCertificate.getPolicyNumber().isEmpty()) {
@@ -148,6 +138,9 @@ public class AccidentStatementService {
                     }
                 }
             }
+
+            policyHolderRepository.saveAll(accidentStatement.getPolicyHolders());
+
             accidentStatementRepository.save(accidentStatement);
 
             return "Accident Statement created";
@@ -156,6 +149,73 @@ public class AccidentStatementService {
 
             log.error("Error creating Accident Statement:", e);
             throw new AccidentStatementCreationFailed("Error occurred while creating Accident Statement");
+        }
+    }
+
+    private void saveWitness(AccidentStatement accidentStatement) throws MissingPropertyException {
+        var witness = accidentStatement.getWitness();
+        if (witness != null) {
+            if (witness.getName() == null || witness.getName().isEmpty() || witness.getAddress() == null || witness.getAddress().isEmpty()) {
+                throw new MissingPropertyException("Witness's name and address cannot be null");
+            }
+            var existingWitness = witnessRepository.findByNameAndAddress(witness.getName(), witness.getAddress());
+            if (existingWitness.isPresent()) {
+                accidentStatement.setWitness(existingWitness.get());
+            } else {
+                witnessRepository.save(witness);
+            }
+        }
+    }
+
+    private void saveDrivers(AccidentStatement accidentStatement) {
+        for (Driver driver : accidentStatement.getDrivers()) {
+            var existingDriver = driverRepository.findByDrivingLicenseNr(driver.getDrivingLicenseNr());
+            if (existingDriver.isPresent()) {
+                var index = accidentStatement.getDrivers().indexOf(driver);
+                accidentStatement.getDrivers().set(index, existingDriver.get());
+            } else {
+                driverRepository.save(driver);
+            }
+        }
+    }
+
+    private void saveAccidentImages(AccidentStatement accidentStatement) {
+        if (accidentStatement.getVehicleAAccidentImages() != null) {
+            accidentImageRepository.saveAll(accidentStatement.getVehicleAAccidentImages());
+        }
+        if (accidentStatement.getVehicleBAccidentImages() != null) {
+            accidentImageRepository.saveAll(accidentStatement.getVehicleBAccidentImages());
+        }
+    }
+
+    private void assignAndMapVehicleDTOsToAccidentStatement(AccidentStatementDTO accidentStatementDTO, AccidentStatement accidentStatement) {
+        List<InsuranceCertificateDTO> policyHolderAInsuranceCertificates = accidentStatementDTO.getPolicyHolders().get(0).getInsuranceCertificates();
+        List<InsuranceCertificateDTO> policyHolderBInsuranceCertificates = accidentStatementDTO.getPolicyHolders().get(1).getInsuranceCertificates();
+
+        // Assign the vehicles of the policyholders accordingly
+        for (int i = 0; i < accidentStatement.getPolicyHolders().size(); i++) {
+            PolicyHolder policyHolder = accidentStatement.getPolicyHolders().get(i);
+            List<InsuranceCertificateDTO> insuranceCertificatesDTO;
+            if (i == 0) {
+                insuranceCertificatesDTO = policyHolderAInsuranceCertificates;
+            } else {
+                insuranceCertificatesDTO = policyHolderBInsuranceCertificates;
+            }
+
+            // Iterate over the InsuranceCertificates and InsuranceCertificateDTOs in parallel
+            for (int j = 0; j < policyHolder.getInsuranceCertificates().size(); j++) {
+                InsuranceCertificate insuranceCertificate = policyHolder.getInsuranceCertificates().get(j);
+                InsuranceCertificateDTO insuranceCertificateDTO = insuranceCertificatesDTO.get(j);
+
+                // Call the mapper to convert VehicleDTO to Vehicle
+                if (insuranceCertificateDTO.getVehicle() instanceof MotorDTO) {
+                    Motor motor = motorMapper.fromDTO((MotorDTO) insuranceCertificateDTO.getVehicle());
+                    insuranceCertificate.setVehicle(motor);
+                } else if (insuranceCertificateDTO.getVehicle() instanceof TrailerDTO) {
+                    Trailer trailer = trailerMapper.fromDTO((TrailerDTO) insuranceCertificateDTO.getVehicle());
+                    insuranceCertificate.setVehicle(trailer);
+                }
+            }
         }
     }
 
