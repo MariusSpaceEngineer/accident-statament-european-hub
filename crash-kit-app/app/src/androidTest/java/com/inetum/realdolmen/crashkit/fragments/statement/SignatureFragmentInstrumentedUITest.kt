9e84344b2b0450db53f5f5d887eeb5558f85bd65
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-04-26 14:05:25 +0200

    1223 Create VehicleTrailerInsuranceFragmentInstrumentedUITest

---
+++ crash-kit-app/app/src/androidTest/java/com/inetum/realdolmen/crashkit/fragments/statement/SignatureFragmentInstrumentedUITest.kt
@@ -0,0 +1,87 @@
+package com.inetum.realdolmen.crashkit.fragments.statement
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import com.inetum.realdolmen.crashkit.R
+import io.mockk.clearMocks
+import io.mockk.mockk
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SignatureFragmentInstrumentedUITest {
+    private lateinit var device: UiDevice
+    private var originalWindowAnimationScale: String = ""
+    private var originalTransitionAnimationScale: String = ""
+    private var originalAnimatorDurationScale: String = ""
+
+    private val mockNavController = mockk<NavController>(relaxed = true)
+
+    //Disable animations on device and set up navController as required
+    @Before
+    fun setup() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        originalWindowAnimationScale =
+            device.executeShellCommand("settings get global window_animation_scale")
+        originalTransitionAnimationScale =
+            device.executeShellCommand("settings get global transition_animation_scale")
+        originalAnimatorDurationScale =
+            device.executeShellCommand("settings get global animator_duration_scale")
+
+        device.executeShellCommand("settings put global window_animation_scale 0")
+        device.executeShellCommand("settings put global transition_animation_scale 0")
+        device.executeShellCommand("settings put global animator_duration_scale 0")
+
+        launchFragmentInContainer<AccidentStatementSignatureFragment>(themeResId = R.style.Theme_CrashKit) {
+            AccidentStatementSignatureFragment().also { fragment ->
+                // In addition to returning a new instance of our Fragment,
+                // get a callback whenever the fragment’s view is created
+                // or destroyed so that we can set the mock NavController
+                fragment.viewLifecycleOwnerLiveData.observeForever { viewLifecycleOwner ->
+                    if (viewLifecycleOwner != null) {
+                        // The fragment’s view has just been created
+                        Navigation.setViewNavController(fragment.requireView(), mockNavController)
+                    }
+                }
+            }
+        }
+    }
+
+    //Restore animations
+    @After
+    fun teardown() {
+        device.executeShellCommand("settings put global window_animation_scale $originalWindowAnimationScale")
+        device.executeShellCommand("settings put global transition_animation_scale $originalTransitionAnimationScale")
+        device.executeShellCommand("settings put global animator_duration_scale $originalAnimatorDurationScale")
+        clearMocks(mockNavController)
+    }
+
+    @Test
+    fun testSignatureUIElements() {
+        // Check to see if the fields are displayed on the screen
+        onView(withId(R.id.sp_statement_vehicle_a))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.btn_statement_vehicle_a_disagree))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.sp_statement_vehicle_b))
+            .perform(scrollTo())
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.btn_statement_vehicle_b_disagree))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.btn_statement_vehicle_a_disagree)).check(matches(ViewMatchers.isClickable()))
+        onView(withId(R.id.btn_statement_vehicle_b_disagree)).check(matches(ViewMatchers.isClickable()))
+    }
+}
