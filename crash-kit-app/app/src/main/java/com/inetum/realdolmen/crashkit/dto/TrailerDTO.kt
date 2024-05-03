b7ae5223645bf38c7bb9a01b19257fe046a92d7b
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-03 11:06:57 +0200

    1226 Update DTOs to correspond with the changes in the TrailerDTO

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/TrailerDTO.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/dto/TrailerDTO.kt
@@ -2,7 +2,8 @@ package com.inetum.realdolmen.crashkit.dto
 
 data class TrailerDTO(
     @Transient override val id: Int?,
-    @Transient override val licensePlate: String,
-    @Transient override val countryOfRegistration: String,
-    val hasRegistration: Boolean
+    @Transient override val licensePlate: String?,
+    @Transient override val countryOfRegistration: String?,
+    val hasRegistration: Boolean,
+    val ofVehicle: String?
 ) : Vehicle(id, licensePlate, countryOfRegistration)
