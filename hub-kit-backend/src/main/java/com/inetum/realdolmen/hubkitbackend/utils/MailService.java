a5d34d91553805c4f87cfbdab7e706156cb5f40a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-06 10:20:03 +0200

    1226 Update sendStatement method in MailService to send the statement accordingly to all the parties

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/utils/MailService.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/utils/MailService.java
@@ -1,5 +1,9 @@
 package com.inetum.realdolmen.hubkitbackend.utils;
 
+import com.inetum.realdolmen.hubkitbackend.models.AccidentStatement;
+import com.inetum.realdolmen.hubkitbackend.models.Driver;
+import com.inetum.realdolmen.hubkitbackend.models.InsuranceCertificate;
+import com.inetum.realdolmen.hubkitbackend.models.PolicyHolder;
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
 import com.mailjet.client.MailjetResponse;
@@ -15,7 +19,6 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashSet;
 import java.util.Set;
@@ -54,44 +57,74 @@ public class MailService {
         return CompletableFuture.completedFuture(null);
     }
 
-    public void sendStatement(File pdfFile, String... recipients) throws MailjetException, IOException {
-
+    public void sendStatement(File pdfFile, AccidentStatement accidentStatement) throws MailjetException, IOException {
         String encodedString = convertPDFFileToBase64(pdfFile);
 
-        // Create a set of recipients to remove duplicates
-        Set<String> recipientSet = new HashSet<>(Arrays.asList(recipients));
+        // Create a set to store insurance agency emails to avoid duplicates
+        Set<String> insuranceEmails = new HashSet<>();
 
-        // Create the TO field for the email
-        JSONArray toField = new JSONArray();
-        for (String recipient : recipientSet) {
-            toField.put(new JSONObject().put("Email", recipient));
+        // Send emails to policyholders, insurance agencies, and drivers
+        for (int i = 0; i < accidentStatement.getPolicyHolders().size(); i++) {
+            PolicyHolder policyHolder = accidentStatement.getPolicyHolders().get(i);
+            Driver driver = accidentStatement.getDrivers().get(i);
+
+            // Send email to policyholder
+            sendEmailWithPDFAttachment(policyHolder.getEmail(), "An accident statement has been created on your name",
+                    "Dear " + policyHolder.getFirstName() + " " + policyHolder.getLastName() + ",\n\nPlease find attached the accident statement for your insurance. If you have any issues or this wasn't intended, please inform your insurance agency.\n\nBest,\nThe CrashKit Team",
+                    "<p>Dear " + policyHolder.getFirstName() + " " + policyHolder.getLastName() + ",<br/>Please find attached the accident statement for your insurance. If you have any issues or this wasn't intended, please inform your insurance agency.</p><p>Best,<br/>The CrashKit Team</p>",
+                    encodedString);
+
+            // Send email to insurance agencies
+            for (InsuranceCertificate insuranceCertificate : policyHolder.getInsuranceCertificates()) {
+                if (!insuranceEmails.contains(insuranceCertificate.getInsuranceAgency().getEmail())) {
+                    sendEmailWithPDFAttachment(insuranceCertificate.getInsuranceAgency().getEmail(), "One of your clients has had an accident",
+                            "Dear Agency,\n\nAn accident statement has been made for one of your clients with the name: " + policyHolder.getFirstName() + " " + policyHolder.getLastName() + ". Please find the details in the attached document.\n\nBest,\nThe CrashKit Team",
+                            "<p>Dear Agency,<br/>An accident statement has been made for one of your clients with the name: " + policyHolder.getFirstName() + " " + policyHolder.getLastName() + ". Please find the details in the attached document.</p><p>Best,<br/>The CrashKit Team</p>",
+                            encodedString);
+                    insuranceEmails.add(insuranceCertificate.getInsuranceAgency().getEmail());
+                }
+            }
+
+            // Send email to driver if the driver's email is not the same as the policyholder's email
+            if (!driver.getEmail().equals(policyHolder.getEmail())) {
+                sendEmailWithPDFAttachment(driver.getEmail(), "An accident statement has been created on your name",
+                        "Dear " + driver.getFirstName() + " " + driver.getLastName() + ",\n\nHere is a copy of the accident statement to which you were part of but were not the policy holder. If there are any discrepancies, please inform your insurance agency.\n\nBest,\nThe CrashKit Team",
+                        "<p>Dear " + driver.getFirstName() + " " + driver.getLastName() + ",<br/>Here is a copy of the accident statement to which you were part of but were not the policy holder. If there are any discrepancies, please inform your insurance agency.</p><p>Best,<br/>The CrashKit Team</p>",
+                        encodedString);
+            }
+
+            // Delete the PDF file
+            if (!pdfFile.delete()) {
+                System.out.println("Failed to delete the file: " + pdfFile.getAbsolutePath());
+            }
         }
+    }
 
+    private void sendEmailWithPDFAttachment(String recipientEmail, String subject, String textPart, String htmlPart, String encodedString) throws MailjetException, IOException {
         MailjetRequest request = new MailjetRequest(Emailv31.resource)
                 .property(Emailv31.MESSAGES, new JSONArray()
                         .put(new JSONObject()
                                 .put(Emailv31.Message.FROM, new JSONObject()
                                         .put("Email", senderEmail)
                                         .put("Name", senderName))
-                                .put(Emailv31.Message.TO, toField)
-                                .put(Emailv31.Message.SUBJECT, "Accident Statement:")
-                                .put(Emailv31.Message.TEXTPART, "Dear user,\n\nPlease find attached the accident statement for your insurance. If you have any questions or need assistance, feel free to reach out to us.\n\nBest,\nThe CrashKit Team")
-                                .put(Emailv31.Message.HTMLPART, "<p>Dear user,<br/>Please find attached the accident statement for your insurance. If you have any questions or need assistance, feel free to reach out to us.</p><p>Best,<br/>The CrashKit Team</p>")
+                                .put(Emailv31.Message.TO, new JSONArray()
+                                        .put(new JSONObject().put("Email", recipientEmail)))
+                                .put(Emailv31.Message.SUBJECT, subject)
+                                .put(Emailv31.Message.TEXTPART, textPart)
+                                .put(Emailv31.Message.HTMLPART, htmlPart)
                                 .put(Emailv31.Message.ATTACHMENTS, new JSONArray()
                                         .put(new JSONObject()
                                                 .put("ContentType", "application/pdf")
-                                                .put("Filename", pdfFile.getName())
+                                                .put("Filename", "Accident Statement") //TODO add the right name
                                                 .put("Base64Content", encodedString)))));
         MailjetResponse response = mailjetClient.post(request);
         System.out.println(response.getStatus());
         System.out.println(response.getData());
-
-        CompletableFuture.completedFuture(null);
     }
+
 
     private static String convertPDFFileToBase64(File pdfFile) throws IOException {
         byte[] fileContent = Files.readAllBytes(pdfFile.toPath());
-        String encodedString = Base64.getEncoder().encodeToString(fileContent);
-        return encodedString;
+        return Base64.getEncoder().encodeToString(fileContent);
     }
 }
