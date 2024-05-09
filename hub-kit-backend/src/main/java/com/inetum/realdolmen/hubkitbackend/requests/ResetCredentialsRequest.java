f51cb2b4845af779adde4e70f4d1c10f5cce96b6
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-09 16:45:34 +0200

    1211 Add Builder annotation to ResetCredentialsRequest

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/requests/ResetCredentialsRequest.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/requests/ResetCredentialsRequest.java
@@ -1,8 +1,10 @@
 package com.inetum.realdolmen.hubkitbackend.requests;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ResetCredentialsRequest {
     private String email;
     private String newPassword;
