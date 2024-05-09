8f94bcfba44f1cb3ef9305f73bfac8bb6880c0a2
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-09 16:45:13 +0200

    1211 Create TestApplicationConfig

---
+++ hub-kit-backend/src/test/java/com/inetum/realdolmen/hubkitbackend/configurations/TestApplicationConfig.java
@@ -0,0 +1,73 @@
+package com.inetum.realdolmen.hubkitbackend.configurations;
+
+import com.inetum.realdolmen.hubkitbackend.repositories.UserRepository;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+@Profile("test")
+public class TestApplicationConfig {
+
+    private final UserRepository repository;
+    @Value("${mailjet.api.key}")
+    private String mailjetApiKey;
+    @Value("${mailjet.secret.key.api}")
+    private String mailjetSecretKey;
+
+
+    @Bean
+    public UserDetailsService testUserDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider testAuthenticationProvider() {
+        DaoAuthenticationProvider authProvide = new DaoAuthenticationProvider();
+        authProvide.setUserDetailsService(testUserDetailsService());
+        authProvide.setPasswordEncoder(testPasswordEncoder());
+        return authProvide;
+    }
+
+    @Bean
+    public AuthenticationManager testAuthenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder testPasswordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return rawPassword.toString().equals(encodedPassword);
+            }
+        };
+    }
+
+    @Bean
+    public MailjetClient testMailjetClient(){
+        ClientOptions options = ClientOptions.builder()
+                .apiKey(mailjetApiKey)
+                .apiSecretKey(mailjetSecretKey)
+                .build();
+        return new MailjetClient(options);
+    }
+}
