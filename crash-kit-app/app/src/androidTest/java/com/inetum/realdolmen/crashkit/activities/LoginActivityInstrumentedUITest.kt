7a4eda6b0ed8f42adca7ad56cb7652778f3f2eaa
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-13 13:51:59 +0200

    Update LoginActivityInstrumentedUITest

--- crash-kit-app/app/src/androidTest/java/com/inetum/realdolmen/crashkit/activities/LoginActivityInstrumentedUITest.kt
+++ crash-kit-app/app/src/androidTest/java/com/inetum/realdolmen/crashkit/activities/LoginActivityInstrumentedUITest.kt
@@ -2,86 +2,66 @@ package com.inetum.realdolmen.crashkit.activities
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.inetum.realdolmen.crashkit.R
-import com.inetum.realdolmen.crashkit.dto.LoginData
-import com.inetum.realdolmen.crashkit.dto.LoginResponse
-import com.inetum.realdolmen.crashkit.services.ApiService
-import com.inetum.realdolmen.crashkit.utils.SecuredPreferences
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.junit4.MockKRule
-import io.mockk.just
-import io.mockk.mockkClass
-import io.mockk.runs
-import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import retrofit2.Response
 
 
 @RunWith(AndroidJUnit4::class)
 class LoginActivityInstrumentedUITest {
-
     @get:Rule
     val activityRule: ActivityScenarioRule<LoginActivity> =
         ActivityScenarioRule(LoginActivity::class.java)
 
-    @get:Rule
-    val mockkRule = MockKRule(this)
-
-    @MockK
-    private lateinit var apiService: ApiService
-
-    @MockK
-    private lateinit var securedPreferences: SecuredPreferences
-
-    @Before
-    fun setUp() {
-        apiService = mockkClass(ApiService::class)
-        securedPreferences = mockkClass(SecuredPreferences::class)
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun testSuccessfulLogin() = runTest {
-        // Given
-        val email = "johnDoe@example.com"
-        val password = "password"
-        val token = "token"
-        val loginResponse = LoginResponse(token, null)
+    fun testLoginActivityUIElements() {
+        // Check if all UI elements are displayed
+        onView(withId(R.id.img_login_background))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.img_login_logo))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.tv_login_app_title))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.tv_login_title))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.et_login_email))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.et_login_password))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.cb_login_remember))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.tv_login_forgot_password))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.btn_login_submit))
+            .check(matches(isDisplayed()))
 
-        // When
-        coEvery { apiService.login(LoginData(email, password)) } returns Response.success(
-            loginResponse
-        )
-        every { securedPreferences.putJwtToken(any()) } just runs
-        every { securedPreferences.getString("jwt_token") } returns loginResponse.token
+        //Check if the buttons are clickable
+        onView(withId(R.id.tv_login_forgot_password))
+            .check(matches(isClickable()))
+        onView(withId(R.id.btn_login_submit))
+            .check(matches(isClickable()))
 
-
-        onView(withId(R.id.et_login_email)).perform(typeText(email))
-        onView(withId(R.id.et_login_password)).perform(
-            typeText(password),
+        // Add text to the EditText fields
+        onView(withId(R.id.et_login_email)).perform(
+            ViewActions.typeText("johndoe@example.com"),
             ViewActions.closeSoftKeyboard()
         )
-        onView(withId(R.id.btn_login_submit)).perform(click())
+        onView(withId(R.id.et_login_password)).perform(
+            ViewActions.typeText("1234"),
+            ViewActions.closeSoftKeyboard()
+        )
 
-        // Then: Verify the expected behavior (e.g., token saved to SharedPreferences)
-        activityRule.scenario.onActivity { activity ->
-
-            // Verify that the token is saved to SecurePreferences
-            val retrievedToken = securedPreferences.getString("jwt_token")
-            assertEquals(token, retrievedToken)
-        }
+        // Assert that the text in the EditText fields is the same as what was typed
+        onView(withId(R.id.et_login_email)).check(matches(ViewMatchers.withText("johndoe@example.com")))
+        onView(withId(R.id.et_login_password)).check(matches(ViewMatchers.withText("1234")))
     }
 }
 
