56eae8c99b688eb0604e5e18e1617f0a040f8b2b
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-13 11:29:51 +0200

    Refactor code of MainActivity

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/activities/MainActivity.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/activities/MainActivity.kt
@@ -6,29 +6,35 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 import com.auth0.android.jwt.JWT
 import com.inetum.realdolmen.crashkit.CrashKitApp
 import com.inetum.realdolmen.crashkit.R
 import com.inetum.realdolmen.crashkit.databinding.ActivityMainBinding
 import com.inetum.realdolmen.crashkit.fragments.LoadingFragment
+import com.inetum.realdolmen.crashkit.utils.SecuredPreferences
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var loadingFragment: LoadingFragment
 
-    private val securedPreferences = CrashKitApp.securedPreferences
+    private lateinit var securedPreferences: SecuredPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // If the JWT token is null, expired, or invalid, initialize the login/register components
+        securedPreferences= CrashKitApp.securedPreferences
+
+        // If the JWT token is null, expired, or invalid, show the MainActivity
         checkLoginStatus()
 
         loadingFragment =
-            supportFragmentManager.findFragmentById(R.id.fr_main_loading) as LoadingFragment
+            supportFragmentManager.findFragmentById(R.id.fr_main_loading) as? LoadingFragment
+                ?: throw RuntimeException("Expected LoadingFragment not found.")
 
         //Changes the color of the last word of the description to red
         changeDescriptionTextToRed()
@@ -41,29 +47,50 @@ class MainActivity : AppCompatActivity() {
         loadingFragment.hideLoadingFragment()
     }
 
-    private fun checkLoginStatus() {
+    //Checks to see if the JWT token of the user is still valid if he has one
+    @VisibleForTesting
+    internal fun checkLoginStatus() {
+        Log.d("LoginStatus", "Checking login status...")
         if (securedPreferences.isLoginRemembered()) {
-
+            Log.d("LoginStatus", "User has remembered login.")
             val jwtToken = securedPreferences.getJwtToken()
             if (jwtToken != null) {
+                Log.d("LoginStatus", "JWT token exists.")
                 val decodedToken = JWT(jwtToken)
-
-                if (!decodedToken.isExpired(10)) {
-                    startActivity(Intent(this, HomeActivity::class.java))
-                    finish()
-                    return
-                }
+                isTokenExpired(decodedToken)
+            } else {
+                Log.d("LoginStatus", "No JWT token found. Remove JWT token.")
+                securedPreferences.deleteJwtToken()
             }
+        } else {
+            Log.d("LoginStatus", "User has not remembered login. Remove any unwanted JWT token.")
+            securedPreferences.deleteJwtToken()
         }
-        securedPreferences.deleteJwtToken()
+    }
+
+    private fun isTokenExpired(decodedToken: JWT) {
+        if (!decodedToken.isExpired(10)) {
+            Log.d("LoginStatus", "JWT token is valid. Navigating to HomeActivity.")
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+            return
+        } else {
+            Log.d("LoginStatus", "JWT token is invalid. Remove JWT token.")
+            securedPreferences.deleteJwtToken()
+        }
     }
 
     private fun changeDescriptionTextToRed() {
-        val spannableText = SpannableString("Save time and hassle after a crash")
+        val text = getString(R.string.main_description)
+        // Locate the position of the last space character in the description text.
+        // We then add 1 to this index to move past the space character.
+        // The resulting 'lastSpaceIndex' points to the first character of the last word in the description text.
+        val lastSpaceIndex = text.lastIndexOf(" ") + 1
+        val spannableText = SpannableString(text)
         spannableText.setSpan(
-            ForegroundColorSpan(Color.RED), // change the color as needed
-            29, // start index
-            34, // end index
+            ForegroundColorSpan(Color.RED),
+            lastSpaceIndex, // start index
+            text.length, // end index
             Spannable.SPAN_INCLUSIVE_INCLUSIVE
         )
         binding.tvMainDescription.text = spannableText
