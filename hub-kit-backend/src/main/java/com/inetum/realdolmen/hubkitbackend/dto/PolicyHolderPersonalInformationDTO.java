c4eaf0f4259afff7bd7bae2b4ea15e5833e76a1d
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-14 15:45:54 +0200

    Add not empty annotation to PolicyHolderPersonalInformationDTO

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/PolicyHolderPersonalInformationDTO.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/PolicyHolderPersonalInformationDTO.java
@@ -1,5 +1,6 @@
 package com.inetum.realdolmen.hubkitbackend.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
@@ -11,15 +12,21 @@ import lombok.Setter;
 public class PolicyHolderPersonalInformationDTO {
     private Integer id;
     @NonNull
+    @NotEmpty
     private String firstName;
     @NonNull
+    @NotEmpty
     private String lastName;
     @NonNull
+    @NotEmpty
     private String email;
     @NonNull
+    @NotEmpty
     private String address;
     @NonNull
+    @NotEmpty
     private String postalCode;
     @NonNull
+    @NotEmpty
     private String phoneNumber;
 }
