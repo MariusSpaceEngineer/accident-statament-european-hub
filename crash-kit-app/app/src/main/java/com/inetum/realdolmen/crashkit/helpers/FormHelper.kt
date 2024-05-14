5e23cf9bb01b26e063bb9f5d62e9bbbbd3e75f3a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-14 09:49:02 +0200

    Add validation rules for passwords in FormHelper

--- crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/helpers/FormHelper.kt
+++ crash-kit-app/app/src/main/java/com/inetum/realdolmen/crashkit/helpers/FormHelper.kt
@@ -1,15 +1,16 @@
 package com.inetum.realdolmen.crashkit.helpers
 
 import android.content.Context
+import android.widget.EditText
 import android.widget.TextView
 import com.inetum.realdolmen.crashkit.R
 
 class FormHelper(
-    context: Context,
+    private val context: Context,
     private val fields: List<TextView>
 ) {
 
-    public val errors = InputFieldsErrors(
+    val errors = InputFieldsErrors(
         context.getString(R.string.field_required),
         context.getString(R.string.no_digits_allowed),
         context.getString(R.string.no_letters_allowed),
@@ -19,12 +20,39 @@ class FormHelper(
         context.getString(R.string.invalid_email)
     )
 
-    fun validateFields(validationRules: List<Triple<TextView, (String?) -> Boolean, String>>) {
+    fun validateFields(validationRules: List<Triple<EditText, (String?) -> Boolean, String?>>) {
         validationRules.forEach { (field, validationRule, errorMessage) ->
             if (validationRule(field.text.toString())) {
                 field.error = errorMessage
             }
         }
+    }
+
+    fun validatePassword(password: String?): String? {
+        val passwordErrors = mutableListOf<String>()
+        if (password.isNullOrEmpty()){
+            passwordErrors.add(errors.fieldRequired)
+        }
+        if ((password?.length ?: 0) < 6) {
+            passwordErrors.add(context.getString(R.string.password_too_short))
+        }
+        if (password?.any { it.isUpperCase() } != true) {
+            passwordErrors.add(context.getString(R.string.password_no_uppercase))
+        }
+        if (password?.any { it.isLowerCase() } != true) {
+            passwordErrors.add(context.getString(R.string.password_no_lowercase))
+        }
+        if (password?.any { it.isDigit() } != true) {
+            passwordErrors.add(context.getString(R.string.password_no_digits))
+        }
+        if (password?.contains(Regex("[@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]")) != true) {
+            passwordErrors.add(context.getString(R.string.password_no_special_character))
+        }
+        if (password?.contains(" ") == true) {
+            passwordErrors.add(context.getString(R.string.password_whitespace))
+        }
+        // If no errors, return null
+        return if (passwordErrors.isEmpty()) null else passwordErrors.joinToString(separator = "\n")
     }
 
     fun clearErrors() {
