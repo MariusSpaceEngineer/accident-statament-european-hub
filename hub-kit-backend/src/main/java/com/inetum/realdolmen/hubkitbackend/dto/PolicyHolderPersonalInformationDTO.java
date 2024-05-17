433b19e3b8c528f6dff387d8a08305af6e5de66a
Marius Stoica <Marius.Stoica@inetum-realdolmen.world>
2024-05-17 04:14:56 +0200

    Update PolicyHolderPersonalInformationDTO property annotations

--- hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/PolicyHolderPersonalInformationDTO.java
+++ hub-kit-backend/src/main/java/com/inetum/realdolmen/hubkitbackend/dto/PolicyHolderPersonalInformationDTO.java
@@ -2,14 +2,12 @@ package com.inetum.realdolmen.hubkitbackend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@EqualsAndHashCode
 public class PolicyHolderPersonalInformationDTO {
     private Integer id;
     @NonNull
